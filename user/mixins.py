036435c0521451ce61927175ee9c9812bab90661
adgforce <adgforce.ceo@gmail.com>
2023-03-08 17:13:01 -0500

    Delete mixins.py

--- user/mixins.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import redirect 
-
-class AdminMixin(object):
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.Administrador:
-            return super().dispatch(request, *args, **kwargs)
-        else:
-            return redirect('dashboardUsuario369')
